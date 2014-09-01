1964fa6a791645ae868aaec4ff97f87db2a6d9ab
taung2630 <genius.thura@gmail.com>
2014-09-01 14:00:05 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+<link rel="Icon" href="images/favicon.ico" >
+<link rel="stylesheet" href="images/main.css" type="text/css" />
+<title>Group 4 Cinema</title>
+
+</head>
+
+<body>
+<div id="wrap">
+	<div id="header">				
+		<h1 id="logo">Group<span class="green">4</span>Cinema<span class="gray"></span></h1>	
+		<h2 id="slogan">Best Cinema in Singapore</h2> 
+		<ul>
+			<li id="current"><a href="index.php"><span>Home</span></a></li>
+			<li><a href="movies.php"><span>Movies</span></a></li>
+            <li><a href="booking.php"><span>Booking</span></a></li>
+            <li><a href="about.php"><span>About</span></a></li>
+            <li><a href="feedback.php"><span>Feedback</span></a></li>
+            <li><a href="contact.php"><span>Contact</span></a></li>
+		</ul>	
+	</div>	
+	<div id="content-wrap">		
+	
+		<div id="sidebar" >							
+			<h1>Useful links</h1>
+            <p align="center"><a href="http://www.imdb.com" target="_blank">IMDB</a></p>
+           	
+		</div>
+		<div id="main">	
+		<h2 align="center">Introduction</h2>
+        <h3>Welcome to The Group 4 Cinema</h3>
+        <p>Group4Cinema is located at the heart of downtown Singapore. We offer the best movie viewing experience for our viewers. Our cinemas are occupied with state of the art sound systems and projectors for a immersive movie experience. </p>
+        
+        
+        
+		</div>	
+		<div id="rightbar">
+        
+		<?php 
+		include('login.php');
+		 ?>  
+			<h1>About Singapore</h1>
+			<p>Group4Cinema is the best cinema in Singapore, showing all the latest movies, serving the best food and offering the best viewing experience, come down and watch movies now or book tickets online!</p>
+		</div>			
+	</div>
+<div id="footer">
+	<div class="footer-left">
+		<p class="align-left">			
+		&copy; 2014 <strong>Group 4 Cinema</strong> |
+		Design by <strong>SP53 2014</strong>
+		
+		</p>		
+	</div>
+	
+	<div class="footer-right">
+		<p class="align-right">
+		<a href="index.php">Home</a>&nbsp;|&nbsp;
+  		<a href="contact.php">Contact</a>
+   	
+		</p>
+	</div>
+	</div>
+</div>
+
+</body>
+</html>
