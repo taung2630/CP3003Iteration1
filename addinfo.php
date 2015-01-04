53e09d28b7ecfed704c99bfc4373c9480e48e82e
taung2630 <genius.thura@gmail.com>
2015-01-04 16:51:13 +0800

    Update addinfo.php

--- addinfo.php
+++ addinfo.php
@@ -6,7 +6,7 @@ session_start();
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
-<link rel="Icon" href="images/favicon.ico" >
+ <link rel="Icon" href="images/favicon.ico" >
 
 <script type="text/javascript">
 
